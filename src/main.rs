c051f5d5245b7f7a54ba066a79d55c874cc0b4e7
marusama <maru.sama@gmail.com>
2020-05-03 01:14:23 +0300

    example 4

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     example1();
     example2();
     example3();
+    example4();
 }
 
 fn example1() {
@@ -60,5 +61,34 @@ struct Game<'a> {
 impl Game<'_> {
     fn level(&self, x: &i32) -> &i32 {
         self.level
+    }
+}
+
+fn example4() {
+    println!("example 4");
+
+    let x = 5;
+    let r;
+
+    {
+        let y = 10;
+        r = x_or_const(&x, &y);
+    }
+
+    println!("r is {}", r);
+}
+
+const DAN: i32 = 241;
+
+struct Coordinate<'a> {
+    x: &'a i32,
+    y: &'a i32,
+}
+
+fn x_or_const<'a, 'b>(x: &'a i32, y: &'b i32) -> &'a i32 {
+    if x > y {
+        x
+    } else {
+        &DAN
     }
 }
