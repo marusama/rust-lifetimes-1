a8fec7d50ae1013b3b50e31f0fff176d9ada17f7
marusama <maru.sama@gmail.com>
2020-05-03 00:29:58 +0300

    example 1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 fn main() {
     println!("Hello, world!");
 
+    example1();
+}
+
+fn example1() {
+    println!("example 1");
+
     let str1 = "1234567890".to_string();
     let str2 = "123".to_string();
 
-    println!("The longest string is {}", longest(&str1, &str2));
+    println!("The longest string is {}", longest1(&str1, &str2));
 }
 
-fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
+fn longest1<'a>(str1: &'a str, str2: &'a str) -> &'a str {
     if str1.len() > str2.len() {
         str1
     } else {
