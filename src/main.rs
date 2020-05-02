fd201cb0b103502e34b77b4a40c1e2af6d5ebfc8
marusama <maru.sama@gmail.com>
2020-05-03 00:26:24 +0300

    longest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    let str1 = "1234567890".to_string();
+    let str2 = "123".to_string();
+
+    println!("The longest string is {}", longest(&str1, &str2));
+}
+
+fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
+    if str1.len() > str2.len() {
+        str1
+    } else {
+        str2
+    }
 }
